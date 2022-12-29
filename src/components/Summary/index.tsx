6c3a95d8b0c17f3ed7eed0a1e0c7c66fffcb7d98
Isadora Brito <isadoradpbrito@gmail.com>
2022-12-29 15:41:08 -0300

    feat: add formatter to date and values

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -2,9 +2,28 @@ import { SummaryCard, SummaryContainer } from "./styles";
 import { ArrowCircleUp, ArrowCircleDown, CurrencyDollar } from "phosphor-react";
 import { useContext } from "react";
 import { TransactionsContext } from "../../contexts/TransactionsContext";
+import { priceFormatter } from "../../utils/formatter";
 
 export function Summary() {
   const { transactions } = useContext(TransactionsContext);
+
+  const summary = transactions.reduce(
+    (acc, transaction) => {
+      if (transaction.type === "income") {
+        acc.income += transaction.price;
+        acc.total += transaction.price;
+      } else {
+        acc.outcome += transaction.price;
+        acc.total -= transaction.price;
+      }
+      return acc;
+    },
+    {
+      income: 0,
+      outcome: 0,
+      total: 0,
+    }
+  );
 
   return (
     <SummaryContainer>
@@ -13,21 +32,21 @@ export function Summary() {
           <span>Entrada</span>
           <ArrowCircleUp size={32} color="#00b37e" />
         </header>
-        <strong>R$ 19.000,00</strong>
+        <strong>{priceFormatter.format(summary.income)}</strong>
       </SummaryCard>
       <SummaryCard>
         <header>
           <span>Saída</span>
           <ArrowCircleDown size={32} color="#f75a68" />
         </header>
-        <strong>R$ 15.000,00</strong>
+        <strong>{priceFormatter.format(summary.outcome)}</strong>
       </SummaryCard>
       <SummaryCard variant="green">
         <header>
           <span>Total</span>
           <CurrencyDollar size={32} color="#fff" />
         </header>
-        <strong>R$ 4.000,00</strong>
+        <strong>{priceFormatter.format(summary.total)}</strong>
       </SummaryCard>
     </SummaryContainer>
   );
